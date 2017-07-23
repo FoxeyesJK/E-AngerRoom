a09e144dbcba901725ca753fbd3e34930673b0d7
Jake <foxeyes92@gmail.com>
2017-07-23 21:22:00 +0000

    update database

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ var express     = require("express"),
     //seedDB      = require("./seeds")
 
 
-mongoose.connect(process.env.DATABASEURL);
+var url = process.env.DATABASEURL || "mongodb://localhost/e_anger_room"
+mongoose.connect(url);
 //mongoose.connect("mongodb://localhost/e_anger_room");
 //mongoose.connect("mongodb://foxeyes:kwonj1jw@ds031541.mlab.com:31541/e-angerroom");
 
