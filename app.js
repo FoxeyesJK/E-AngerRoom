065fd7b73b353d033238929b0282a2b26886a349
Jake <foxeyes92@gmail.com>
2017-08-02 21:27:45 +0000

    deploy

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ var express     = require("express"),
     //seedDB      = require("./seeds")
 
 
-//var url = process.env.DATABASEURL || "mongodb://localhost/e_anger_room"
-//mongoose.connect(url);
+var url = process.env.DATABASEURL || "mongodb://localhost/e_anger_room"
+mongoose.connect(url);
 //mongoose.connect("mongodb://localhost/e_anger_room");
 mongoose.connect("mongodb://foxeyes:kwonj1jw@ds031541.mlab.com:31541/e-angerroom");
-
+//
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
