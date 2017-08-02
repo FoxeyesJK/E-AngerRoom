a0b6ad50a89d614f294a10a029a45d732d34bd42
Jake <foxeyes92@gmail.com>
2017-08-02 21:12:53 +0000

    deploy

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var express     = require("express"),
 //mongoose.connect(url);
 //mongoose.connect("mongodb://localhost/e_anger_room");
 mongoose.connect("mongodb://foxeyes:kwonj1jw@ds031541.mlab.com:31541/e-angerroom");
-//
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
